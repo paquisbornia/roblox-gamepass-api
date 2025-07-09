236ea1a4083f846a883cb9d6f7e186d15bfef5d2
paquisbornia <mathiaskstocker@gmail.com>
2025-07-09 19:22:12 -0300

    Added the main code and the requirements

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+def get_gamepasses(user_id):
+    all_gamepasses = []
+
+    r = requests.get(f"https://games.roproxy.com/v2/users/{user_id}/games?accessFilter=2&sortOrder=Asc&limit=10")
+    if r.status_code != 200:
+        return []
+
+    data = r.json().get("data", [])
+    for game in data:
+        universe_id = game.get("id")
+        if not universe_id:
+            continue
+        gp_request = requests.get(
+            f"https://games.roblox.com/v1/games/{universe_id}/game-passes?limit=100&sortOrder=1"
+        )
+        if gp_request.status_code != 200:
+            continue
+
+        gamepasses = gp_request.json().get("data", [])
+        for gp in gamepasses:
+            asset_id = gp["id"]
+
+            all_gamepasses.append(asset_id)
+
+    return all_gamepasses
+
+@app.route("/api/gamepasses", methods=["GET"])
+def api_gamepasses():
+    user_id = request.args.get("userId")
+    
+    if not user_id or not user_id.isdigit():
+        return jsonify({"error": "Parâmetro 'userId' ausente ou inválido"}), 400
+
+    data = get_gamepasses(user_id)
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
