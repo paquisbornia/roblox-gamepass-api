23a74f28457de15fb0016c9b410ddd48c3ca0a54
paquisbornia <mathiaskstocker@gmail.com>
2025-12-02 21:46:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,39 +12,31 @@ def get_gamepasses(user_id):
         return []
 
     data = r.json().get("data", [])
-    print("Data: ", data)
     for game in data:
-        print("Game: ", game)
         universe_id = game.get("id")
         if not universe_id:
             continue
         gp_request = requests.get(
             f"https://apis.roblox.com/game-passes/v1/universes/{universe_id}/game-passes?passView=Full&pageSize=100"
         )
-        print(gp_request)
         if gp_request.status_code != 200:
             continue
-        print("Working")
         gamepasses = gp_request.json()
         gamepasses = gamepasses.get("gamePasses", [])
         for gp in gamepasses:
-            print(gp)
             asset_id = gp["id"]
 
             all_gamepasses.append(asset_id)
 
-    print(all_gamepasses)
     return all_gamepasses
 
 @app.route("/api/gamepasses", methods=["GET"])
 def api_gamepasses():
     user_id = request.args.get("userId")
-    print(user_id)
     if not user_id or not user_id.isdigit():
         return jsonify({"error": "Parâmetro 'userId' ausente ou inválido"}), 400
 
     data = get_gamepasses(user_id)
-    print(data)
     return jsonify(data)
 
 if __name__ == "__main__":
